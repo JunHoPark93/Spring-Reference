abecf0ddcca5f7766936df29249532a1b6263600
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-27 11:42:20 +0900

    Initial Commit

---
+++ src/main/java/com/jaytech/springreference/SpringReferenceApplication.java
@@ -0,0 +1,14 @@
+package com.jaytech.springreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReferenceApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/jaytech/springreference/SpringReferenceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jaytech.springreference;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringReferenceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
