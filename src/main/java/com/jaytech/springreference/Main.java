6fd44beec264c7de451a50120045ba523494ee06
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-27 17:43:14 +0900

    Create test for sample controller, service (which will be mocked in test)

---
+++ src/main/java/com/jaytech/springreference/Main.java
@@ -0,0 +1,18 @@
+package com.jaytech.springreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Main.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+
+        application.run(args);
+    }
+
+}
+
--- src/test/java/com/jaytech/springreference/SpringReferenceApplicationTests.java
+++ src/test/java/com/jaytech/springreference/SpringReferenceApplicationTests.java
@@ -2,8 +2,13 @@ package com.jaytech.springreference;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,7 @@ public class SpringReferenceApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
