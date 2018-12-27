0d05aae6a66662c8ac949bf2aeaa25185f9ef0f2
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-27 17:52:09 +0900

    Create mocking test

--- src/test/java/com/jaytech/springreference/sample/SampleControllerTest.java
+++ src/test/java/com/jaytech/springreference/sample/SampleControllerTest.java
@@ -5,28 +5,27 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class SampleControllerTest {
 
-    @Autowired
-    MockMvc mockMvc;
+    @MockBean
+    SampleService mockSampleService;
 
     @Test
     public void hello_테스트() throws Exception{
-        mockMvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("hellojay"))
-                .andDo(print());
+        when(mockSampleService.getName()).thenReturn("hellojay");
     }
 }
