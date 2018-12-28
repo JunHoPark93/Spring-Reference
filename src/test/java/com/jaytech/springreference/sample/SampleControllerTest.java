357328eedf12a5c7bdaec235c78e47fdeb95bc61
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-28 20:47:06 +0900

    Add mock mvc

--- src/test/java/com/jaytech/springreference/sample/SampleControllerTest.java
+++ src/test/java/com/jaytech/springreference/sample/SampleControllerTest.java
@@ -1,15 +1,18 @@
 package com.jaytech.springreference.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -17,15 +20,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
     @MockBean
-    SampleService mockSampleService;
+    SampleService sampleService;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     public void hello_테스트() throws Exception{
-        when(mockSampleService.getName()).thenReturn("hellojay");
+        when(sampleService.getName()).thenReturn("hellojay");
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("hellohellojay"));
+
+        assertThat(outputCapture.toString()).contains("hello");
     }
 }
